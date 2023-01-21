d3b580e2e4d10222d9f9af0c4e94a90a182a6b50
Sreekar <srikar4492@gmail.com>
2023-01-21 13:35:02 -0500

    My first commit

---
+++ src/MyFirstJavaClass.java
@@ -0,0 +1,26 @@
+
+public class MyFirstJavaClass {
+	
+	public static void main(String args[]){
+		System.out.println("My First Java Output");
+		System.out.println("My Second line");
+		
+		String firstName;
+		String lastName;
+		
+		firstName="Sreekar";
+		lastName="Korlapati";
+		
+		System.out.println(firstName+" "+lastName);
+		
+		int a=10;
+		int b=30;
+		int Total = a+b;
+		
+		System.out.println("Total ->"+Total);
+		System.out.println("Total ->"+(a+b));
+
+			
+	}
+
+}
